e3f372d5cf8be75ae595a0569195d969376644d4
Aaron <ajonaaronajon@yahoo.com>
2021-12-22 23:39:28 -0600

    added ability to add due dates to task form

--- src/taskCreateForm.js
+++ src/taskCreateForm.js
@@ -5,6 +5,7 @@ export const taskCreateForm = ()=>{
     const _form = form("#", "#", "task-create");
     const _taskName = formInput("task-title", "text", "Title: ");
     const _taskDescription = formInput("task-description", "text", "Description: ");
+    const _taskDate = formInput("task-date", "date", "Date: ");
     const _submit = formInput("submit", "submit");
     const _cancel = formInput("cancel","button", "Cancel");
 
@@ -12,6 +13,7 @@ export const taskCreateForm = ()=>{
     _form.addInput(_taskDescription.get());
     _form.addInput(_submit.get());
     _form.addInput(_cancel.get());
+    _form.addInput(_taskDate.get());
 
     const get = ()=> _form.get();
     return {
